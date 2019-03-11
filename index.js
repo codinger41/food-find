10e9233d899df7d60a99d8f4f8f3696e1ea23610
leksyib <leksyib14@gmail.com>
2019-03-11 17:42:34 +0100

    fix react build path

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
   })
 }
 
